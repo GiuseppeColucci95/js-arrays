83a715898dedf26562d128973da0f464e5437d45
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-12 15:25:12 +0100

    add fifth (last) bonus exercise

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -40,4 +40,5 @@ console.log(isFabioPresent);
 console.log("Fabio si trova in posizione: " + teachers.indexOf("Fabio"));
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.toString();
+console.log(teachersString);
